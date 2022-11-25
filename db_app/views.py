5db9feaf16417bb02d9073ceea32c3fcfedd52c6
tanzawua2 <vm9702019@gmail.com>
2022-11-25 15:01:23 -0500

    44444444s

--- db_app/views.py
+++ db_app/views.py
@@ -3,10 +3,13 @@ from rest_framework import viewsets
 from .serializers import PersonaSerializers
 from .models import Persona
 from django.http import HttpResponse
+from rest_framework import viewsets, permissions
 # Create your views here.
 def hola(request):
     return HttpResponse('<h1>HOLA</h1>')
 
 class PersonaViewsets(viewsets.ModelViewSet):
     queryset = Persona.objects.all()
+    permission_classes = [permissions.AllowAny]
     serializer_class = PersonaSerializers
+
